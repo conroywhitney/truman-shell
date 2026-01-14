2378c37ec68a59c6ed507c6cf44214ddfe9f9e55
Conroy Whitney <conroy.whitney@gmail.com>
2026-01-13 23:45:44 -0500

    build: Add Credo for static code analysis
    Add credo ~> 1.7 as dev/test dependency for enforcing
    consistent code style and catching common issues.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule TrumanShell.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.39", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.39", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
